909a3cb49540ad66bdd58c19a660b845159a076f
Mahapara <mahanizamani12345678@gmail.com>
2024-02-16 22:15:26 +0500

    api added to bus Search component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,19 @@
 import AdminPanel from "./Pages/AdminPanel";
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
-import { Header } from "./components";
-import { Footer } from "flowbite-react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Footer, Header } from "./components";
 import HomePage from "./homepage/HomePage";
 
 function App() {
   return (
     <>
-        <BrowserRouter>
-        <Header/>
+      <BrowserRouter>
+        <Header />
         <Routes>
-          <Route path="/"  element={<HomePage />} />
-          <Route path="/AdminPanel"  element={<AdminPanel />} />
-
+          <Route path="/" element={<HomePage />} />
+          <Route path="/AdminPanel" element={<AdminPanel />} />
         </Routes>
-        <Footer/>
-        </BrowserRouter>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
--- frontend/src/Pages/AdminPanel.jsx
+++ frontend/src/Pages/AdminPanel.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
-import ButtonGroup from '../components/ButtonGroup'
- 
-
+import React from "react";
+import ButtonGroup from "../Components/ButtonGroup";
 
 const AdminPanel = () => {
   return (
-    <div >
-      <h1 className='text-center text-xl bg-black text-white py-5 px-5 '>Admin Panel</h1>
+    <div>
+      <h1 className="text-center text-xl bg-black text-white py-5 px-5 ">
+        Admin Panel
+      </h1>
 
       {/*  Buttons Section   */}
-      <div  className='mt-5 text-center'>
+      <div className="mt-5 text-center">
         <ButtonGroup />
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default AdminPanel
+export default AdminPanel;
